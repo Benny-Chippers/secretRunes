fe667469dd263119a62837ac1a559e26601c5b89
amermand <amermand@oregonstate.edu>
2026-04-27 21:24:21 -0700

    QSPI dev

--- NB_firmware/NB_firmware.ino
+++ NB_firmware/NB_firmware.ino
@@ -1,11 +1,12 @@
 /* Author: Damian Amerman-Smith
  * Northbridge MCU's Firmware. 
  *
+ * If having "invalid header" issues, unplug board power for ~3 seconds, then hit reset. 
+ *
+ *
  * TODO (top-to-bottom):
- * Add memory drivers and use them to fulfill memory requests
-    * SD Card driver works, CPU interfacing
  * Use interrupts to handle incoming NB and SB requests
-    * Add command/address bits to communication to sort memory requests
+   * Add command/address bits to communication to sort memory requests
     * Need to add prefix scraper to extract cmd and addr bits from msgs
  * Post-Initial Checkoff:
     * Implement PSRAM, Flash drivers and API handlers for a given memory transaction
@@ -16,7 +17,8 @@
 // Libraries
 #include <Arduino.h>
 #include <string.h>
-#include <driver/spi_master.h>
+// #include <driver/spi_master.h>
+#include <driver/spi_slave.h>
 #include <driver/uart.h>
 #include <esp_random.h>
 #include <sdmmc_cmd.h>
@@ -41,7 +43,7 @@
 
 // Pins for Northbridge-Southbridge UART
 #define UART_TX 33    // old NB/MB: 16, big board: 33
-#define UART_RX 32    // old NB/MB: 17, big board: 33
+#define UART_RX 32    // old NB/MB: 17, big board: 32
 #define UART_PORT UART_NUM_2
 #define UART_BAUD 57600      // Baud Rate (symbols/sec) for NB-SB UART
 SoftwareSerial SBuart(UART_RX, UART_TX);
@@ -52,8 +54,8 @@ SoftwareSerial SBuart(UART_RX, UART_TX);
 #define HSPI_MOSI 13
 #define HSPI_MISO 12
 #define HSPI_CS_SD 26  // old NB/MB: 4, big board: 26
-#define HSPI_CS_FL 27   // old NB/MB: 15, big board: 27
-#define HSPI_CS_PS 15   // old NB/MB: 2, big board: 15
+#define HSPI_CS_FL 15   // old NB/MB: 15, big board: 27
+#define HSPI_CS_PS 2   // old NB/MB: 2, big board: 15
 #define FL_FREQ 40000000 // in Hz
 #define PS_FREQ FL_FREQ
 
@@ -77,7 +79,7 @@ uint8_t UART_buf[BUF_SIZE];
 // For VSPI Configuration
 spi_host_device_t cpu_host = SPI2_HOST;
 spi_bus_config_t cpu_bus;
-// spi_interface_config_t peripheral_config;
+spi_slave_interface_config_t peripheral_config;
 spi_dma_chan_t dma_config = SPI_DMA_DISABLED;
 int msg_idx;
 
@@ -93,9 +95,12 @@ sdspi_device_config_t sd;
 sdspi_dev_handle_t sd_handle;
 spi_bus_config_t bus_cfg;
 
-SPIFlash flash(HSPI_CS_FL, &SPI);
+
+SPIClass* fl = NULL;
+SPIClass* ps = NULL;
+SPIFlash flash(HSPI_CS_FL);
 uint64_t FL_MAX;
-SPIFlash psram(HSPI_CS_PS, &SPI);
+SPIFlash psram(HSPI_CS_PS);
 uint64_t PS_MAX;
 uint32_t ret;
 
@@ -126,7 +131,8 @@ bool init_uart(const uart_port_t port, const uint32_t tx, const uint32_t rx) {
   return true;
 }
 
-// Initializes quad SPI communication
+
+// Initializes quad SPI communication.
 bool init_qspi(spi_host_device_t host, spi_bus_config_t spi_bus, int d0, int d1, int d2, int d3, int clk, int cs) {
   spi_bus.data0_io_num = d0;
   spi_bus.data1_io_num = d1;
@@ -135,15 +141,27 @@ bool init_qspi(spi_host_device_t host, spi_bus_config_t spi_bus, int d0, int d1,
   spi_bus.sclk_io_num = clk;
   spi_bus.quadwp_io_num = -1;
   spi_bus.quadhd_io_num = -1;
-
+  
+  peripheral_config = {
+    .spics_io_num = cs,
+    .flags = 0,
+    .queue_size = 1024,
+    .mode = 0,
+    .post_setup_cb = 0,
+    .post_trans_cb = 0,
+  };
+  
   if (ESP_OK != spi_bus_initialize(host, &spi_bus, SPI_DMA_DISABLED)) {
-    Serial.println("Error: Couldn't initialize SPI bus in peripheral mode");
+    Serial.println("Error: Couldn't initialize NB as a QSPI peripheral");
     return false;
-  } else {
-    return true;
   }
-}
 
+  if (ESP_OK != spi_slave_initialize(host, &spi_bus, &peripheral_config, SPI_DMA_DISABLED)) {
+    Serial.println("Error: Couldn't initialize NB as a QSPI peripheral");
+    return false;
+  } 
+  return true;
+}
 
 
 // Initializes single SPI communication
@@ -161,7 +179,6 @@ bool init_spi(spi_host_device_t host, spi_bus_config_t spi_bus, int mosi, int mi
     return true;
   }
 }
-
 
 
 // // Queues a variable length SPI message to CPU. payload is message to send, length is bytes to send
@@ -323,10 +340,11 @@ size_t read_SD2buf(const char filepath[], size_t size, uint8_t* buf) {
 }
 
 
-// Reads an mp3 file to a byte steam, then sends it to the Southbridge via UART.
+// Reads an ~~mp3~~ (current WAV) file to a byte steam, then sends it to the Southbridge via UART.
 // Buffer will be internally created and is deleted before the end of the function
-// Will return status: 0 is failure, otherwise number of bytes sent 
-size_t send_MP3(const char filepath[]) {
+// Will return status: 0 is failure, otherwise number of bytes sent.
+// Timeout is delay after failure, in seconds
+size_t send_MP3(const char filepath[], double timeout) {
   if (SD.exists(filepath)) {
     size_t size = get_SD_size(filepath);                      // Opens file
     size_t sent = 0;
@@ -356,6 +374,7 @@ size_t send_MP3(const char filepath[]) {
     return size;
   } else {
     Serial.printf("Error: %s not found\n", filepath);
+    delay(timeout*1000);
     return 0;
   }
 
@@ -363,7 +382,7 @@ size_t send_MP3(const char filepath[]) {
 
 
 // Timeout is number of seconds it'll try (every 2.5 sec)
-bool SD_init(double timeout) {
+bool init_SD(double timeout) {
   SPI.begin(HSPI_CLK, HSPI_MISO, HSPI_MOSI, HSPI_CS_SD);
   double elapsed = 0;
   while (!SD.begin(HSPI_CS_SD) & (elapsed < timeout)) {                         // Tries to connect for 7.5 seconds
@@ -421,7 +440,6 @@ void FL_readChar(uint32_t start, uint32_t end) {
 
 void setup() {
   Serial.begin(115200);
-  pinMode(0, OUTPUT);
   randomSeed(time(NULL));
   delay(1);
 
@@ -434,21 +452,24 @@ void setup() {
   Serial.println("HSPI Init...");
   
   // Initializing flash memory
+
+  // External SPI is causing an issue with internal flash when resetting
+  // TODO: to turn SPI on when needed but off when done
   SPI.begin(HSPI_CLK, HSPI_MISO, HSPI_MOSI, HSPI_CS_FL);
-  ret = flash.begin(16000000);                     
+  ret = flash.begin(MB(16));
   Serial.printf("\nFlash Init: %d\n", ret);
-  flash.setClock(FL_FREQ);
   FL_MAX = flash.getCapacity();
+  // flash.setClock(FL_FREQ);
   Serial.printf("%d kB Capacity\n", FL_MAX/1000);
   
-  // Initalizing PSRAM (in-progress)
-  SPI.begin(HSPI_CLK, HSPI_MISO, HSPI_MOSI, HSPI_CS_PS);
-  ret = psram.begin(8000000);                     
-  PS_MAX = psram.getCapacity();
-  Serial.printf("\nPSRAM Init: %d, cap: %d\n", ret, PS_MAX);
-  psram.setClock(PS_FREQ);
-  char str[] = "This is a sample string"; 
-  psram.writeCharArray(0, str, strlen(str));
+  // // Initalizing PSRAM (in-progress, not yet working)
+  // ps->begin(HSPI_CLK, HSPI_MISO, HSPI_MOSI, HSPI_CS_PS);
+  // ret = psram.begin(8000000);                     
+  // PS_MAX = psram.getCapacity();
+  // Serial.printf("\nPSRAM Init: %d, cap: %d\n", ret, PS_MAX);
+  // psram.setClock(PS_FREQ);
+  // char str[] = "This is a sample string"; 
+  // psram.writeCharArray(0, str, strlen(str));
 
   // PSRAM allocation test
   void* norm = NULL;
@@ -456,36 +477,45 @@ void setup() {
   Serial.printf("norm: %x\n", (int) norm);
   free(norm);
 
-  // SD initialization
-  double timeout = 0;       // Time to wait on SD card
-  Serial.printf("\nSD Init: %d\n", SD_init(timeout));
+  // // SD initialization
+  // double timeout = 0;       // Time to wait on SD card
+  // Serial.printf("\nSD Init: %d\n", init_SD(timeout));
 
-  void* ps = NULL;
-  if (!psramInit()) {
-    Serial.println("Couldn't init psram");
-    delay(1000);
-  }
-  ps = ps_malloc(10*sizeof(int));
-  Serial.printf("ps: %x\n", (int) ps);
-  free(ps);
-
+  // void* ps = NULL;
+  // if (!psramInit()) {
+  //   Serial.println("Couldn't init psram");
+  //   delay(1000);
+  // }
+  // ps = ps_malloc(10*sizeof(int));
+  // Serial.printf("ps: %x\n", (int) ps);
+  // free(ps);
 
   // delay(2500)
   // heap_caps_print_heap_info(MALLOC_CAP_8BIT);
   // heap_caps_malloc_extmem_enable(2048);
   // heap_caps_print_heap_info(MALLOC_CAP_SPIRAM);
 
+  // Serial.printf("Init QSPI: %d\n", init_qspi(cpu_host, cpu_bus, VSPI_MOSI, VSPI_MISO, VSPI_D2, VSPI_D3, VSPI_CLK, VSPI_CS));
+
+  // Flash read/write test
+  char str2[] = "This is a sample string";
+
+  FL_clear();
+  delay(1000);
+  Serial.printf("Sample Text Write: %d\n", flash.writeCharArray(0, str2, strlen(str2)));
+  delay(100);
+  FL_readChar(0, strlen(str2));
+  SPI.end();
+
+  // delay(100);
+
+  FL_readChar(0, strlen(str2));
+
+  SPI.begin(HSPI_CLK, HSPI_MISO, HSPI_MOSI, HSPI_CS_FL);
 
 
-
-
+  FL_readChar(0, strlen(str2));
   
-  // // Flash read/write test
-  // char str2[] = "This is a sample string";
-  // FL_clear();
-  // Serial.printf("Sample Text Write: %d\n", flash.writeCharArray(0, str2, strlen(str2)));
-  // FL_readChar(0, strlen(str2));
-
   Serial.println("\nNorthbridge initialized");
 }
 
@@ -498,32 +528,35 @@ void loop() {
   
   spi_transaction_t message;        // Transaction struct
   memset(&message, 0, sizeof(message));
-  
-  int idx = random(0, NUM_WAV-1);
-  delay(1);
-  send_MP3(wavs[idx]);
+    
+  // Tests NB-SB UART Music streaming
+  {
+    int idx = random(0, NUM_WAV-1);
+    delay(1);
+    send_MP3(wavs[idx], 2.5);
+  }
 
-  // message = {
-  //   .flags = 0,
-  //   .length = MSG_SIZE,
-  //   .trans_len = MSG_SIZE,
-  //   .tx_buffer = (void*) &TX_buf,
-  //   .rx_buffer = (void*) &RX_buf,
-  //   .user = (void*) msg_idx++,
-  // };
+  message = {
+    .flags = 0,
+    .length = MSG_SIZE,
+    .trans_len = MSG_SIZE,
+    .tx_buffer = (void*) &TX_buf,
+    .rx_buffer = (void*) &RX_buf,
+    .user = (void*) msg_idx++,
+  };
 
-  // // Queues message
-  // spi_slave_queue_trans(host, &message, portMAX_DELAY);
+  // Queues message
+  spi_slave_queue_trans(host, &message, portMAX_DELAY);
 
-  // // Currently, this holds until the SPI transaction completes (polling)
-  // // May implement multi-core usage/communication interrupts later
-  // if(ESP_OK != spi_slave_get_trans_result(host, (spi_slave_transaction_t**)&message, portMAX_DELAY)) {
-  //   Serial.println("Error: couldn't receive message");
-  //   return;
-  // }
-  // // Prints received data
-  // Serial.print("SPI CPU: ");
-  // Serial.println((char*)RX_buf);
+  // Currently, this holds until the SPI transaction completes (polling)
+  // May implement multi-core usage/communication interrupts later
+  if(ESP_OK != spi_slave_get_trans_result(host, (spi_slave_transaction_t**)&message, portMAX_DELAY)) {
+    Serial.println("Error: couldn't receive message");
+    return;
+  }
+  // Prints received data
+  Serial.print("SPI CPU: ");
+  Serial.println((char*) RX_buf);
 
   // // Tests UART NB->SB Communication
   // memset(UART_buf, '\0', BUF_SIZE);
